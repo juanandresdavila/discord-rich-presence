4227903ed1e8094b26beeb3798b55651c3bde00b
Juan Andres Davila <zad4ytv@gmail.com>
2021-01-27 03:45:04 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af9370' --Replace 0 with your manifest version of your server.
 
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
 client_script 'client.lua'
